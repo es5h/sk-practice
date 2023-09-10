f8344beeff1a5f96936fec15cd108e0cd3d8ba2c
Eusneok Oh <lspk.lpg@gmail.com>
2023-09-10 16:34:48 +0900

    Native Root

---
+++ skpractice/Plugins/MathPlugin/Math.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace skpractice.Plugins.MathPlugin;
+
+public class Math
+{
+    [SKFunction, Description("Take the square root of a number")]
+    public string Sqrt(string number) =>
+        System.Math.Sqrt(Convert.ToDouble(number, CultureInfo.InvariantCulture)).ToString(CultureInfo.InvariantCulture);
+}
