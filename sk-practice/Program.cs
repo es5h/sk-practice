1884248378b697ab91ec4017ca9d8935602de164
Eusneok Oh <lspk.lpg@gmail.com>
2023-09-10 16:25:07 +0900

    Summarize Conversation

--- sk-practice/Program.cs
+++ sk-practice/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.SemanticFunctions;
-using static Microsoft.SemanticKernel.SemanticFunctions.PromptTemplateConfig;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Skills.Core;
 
 var loggerFactory = LoggerFactory.Create(builder =>
 {
@@ -30,9 +30,29 @@ var pluginsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
 
 var orchestratorPlugin = kernel
     .ImportSemanticSkillFromDirectory(pluginsDirectory, "OrchestratorPlugin");
+var conversationSummaryPlugin = kernel.ImportSkill(new ConversationSummarySkill(kernel), "ConversationSummarySkill");
+
+var variables = new ContextVariables
+{
+    ["input"] = "Yes",
+    ["history"] = """
+                  Bot: 무엇을 도와드릴까요?
+                  User: 오늘 날씨가 어때요?
+                  Bot: 현재 위치가 어디세요?
+                  User: 서울입니다.
+                  Bot: 오늘 서울 날씨는 섭씨 70도입니다.
+                  User: 고맙습니다.
+                  Bot: 천만에요.
+                  User: 오늘 캘린더에서 일정알려주세요
+                  Bot: 오후 2시에 팀미팅 있습니다.
+                  User: 우리 팀이 큰 성과를 달성해서 축하 이메일을 보내겠습니다..
+                  Bot: "제가 대신 이메일을 작성해 드릴까요?",
+                  """,
+    ["options"] = "이메일 발송, 이메일 수신, 미팅 발송, 미팅 수신, 채팅 발송",
+};
 
 var result = await kernel.RunAsync(
-    "es5h github의 sk-practice 에 pr을 날린다.",
+    variables,
     orchestratorPlugin["GetIntent"]
 );
 
